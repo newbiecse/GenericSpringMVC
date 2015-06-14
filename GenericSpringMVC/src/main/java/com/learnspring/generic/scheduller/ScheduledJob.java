8dc8bfb4a0c5d61f6412ed110dc868ab0c93b14e
newbiecse <newbiecse@gmail.com>
2015-06-15 00:05:48 +0700

    q

---
+++ GenericSpringMVC/src/main/java/com/learnspring/generic/scheduller/ScheduledJob.java
@@ -0,0 +1,22 @@
+package com.learnspring.generic.scheduller;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class ScheduledJob extends QuartzJobBean{
+	 
+    
+    private AnotherBean anotherBean; 
+     
+     
+    @Override
+    protected void executeInternal(JobExecutionContext arg0)
+            throws JobExecutionException {
+        anotherBean.printAnotherMessage();
+    }
+ 
+    public void setAnotherBean(AnotherBean anotherBean) {
+        this.anotherBean = anotherBean;
+    }
+}
