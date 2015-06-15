c42c59b7527725d2bb70d4db595617d59c4090ec
newbiecse <newbiecse@gmail.com>
2015-06-15 17:01:07 +0700

    quartz scheduller send mail

--- GenericSpringMVC/src/main/java/com/learnspring/generic/scheduller/MyBean.java
+++ GenericSpringMVC/src/main/java/com/learnspring/generic/scheduller/MyBean.java
@@ -1,12 +1,30 @@
 package com.learnspring.generic.scheduller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import com.learnspring.generic.comon.MailMail;
 
 @Component("myBean")
 public class MyBean {
  
+	private MailMail mailMail;
+	
+	@Autowired(required=true)
+	@Qualifier(value="mailMail")		
+	public void setMailMail(MailMail mailMail) {
+		this.mailMail = mailMail;
+	}	
+
+	public void senMail() {
+		mailMail.sendMail("dear", "hello");
+	}
+	
     public void printMessage() {
+    	mailMail.sendMail("dear", "hello");
         System.out.println("I am called by MethodInvokingJobDetailFactoryBean using SimpleTriggerFactoryBean");
+        
     }
      
 }
